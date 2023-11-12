f5a0e468ac9c30067135e679fdfafb68d26c6bed
Arseniy <125693402+Arsentosh@users.noreply.github.com>
2023-11-12 12:18:26 +0200

    Initial commit

---
+++ src/components/Filter.js
@@ -0,0 +1,16 @@
+import { FilterDiv, FilterInput, FilterLabel } from './Filter.style';
+
+export const Filter = ({ filter, onChange }) => {
+  return (
+    <FilterDiv>
+      <FilterLabel htmlFor="filter">Find contacts by name</FilterLabel>
+      <FilterInput
+        type="text"
+        name="filter"
+        id="filter"
+        value={filter}
+        onChange={e => onChange(e.target.value)}
+      />
+    </FilterDiv>
+  );
+};
